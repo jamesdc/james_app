0b045c8c8e47bcbd00057a3f7c68c409b6e5c752
james dela cruz <jamesmartindelacruz@yahoo.com>
2011-04-07 15:41:39 +0800

    intial commit

---
+++ james_project/james_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamesApp::Application.config.secret_token = 'e2425190bb556412a6c1cb4abfe5bb7ff55e431a152dc05bf6b8366d09c7636e158052f7b8980d83f99c6387b39edbc2f9bf15c42330be62d0090ce901ad7bb1'
